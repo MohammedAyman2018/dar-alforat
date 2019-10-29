16d62e6182b1c8f48c05ae739e356a395b98ea67
MohammedAyman2018 <emidobibo20@gmail.com>
2019-10-29 20:54:58 +0200

    Fixing bugs

--- js/app.js
+++ js/app.js
@@ -1,36 +1,69 @@
-
-window.onload = function (){
-    Animate();    
-    ScrollOut();
-}
+// window.onload = function (){
+//     Animate();    
+//     ScrollOut();
+// }
 
 var collapseBtn = document.getElementById('toggle'),
-    collapseDiv = document.getElementById('collapseable');
+    collapseDiv = document.getElementById('collapseable'),
+    nav = document.querySelector('nav'),
 
-collapseBtn.onclick = function (){
-    console.log(collapseDiv.offsetWidth )
+    navLinks = document.querySelectorAll('nav .links ul li a:not(.active) , nav i');
+
+window.onscroll = function () {
+    if (window.scrollY <= 100) {
+        navLinks.forEach(link => {
+            link.style.color = "white";
+        });
+
+        move(nav)
+            .set('background', 'transparent')
+            .duration('0.7s')
+            .end();
+
+        move(collapseDiv)
+            .set('background', 'black')
+            .duration('0.7s')
+            .end()
+    } else {
+        navLinks.forEach(link => {
+            link.style.color = "black";
+        });
+        move(nav)
+            .set('background', '#E7E8EA')
+            .duration('0.7s')
+            .end();
+        move(collapseDiv)
+            .set('background', '#E7E8EA')
+            .duration('0.7s')
+            .end()
+    }
+}
+
+
+collapseBtn.onclick = function () {
+    console.log();
     if (collapseDiv.style.transform !== 'translateX(0px)')
         move(collapseDiv)
             .x(0)
             .set('background', 'black')
             .duration('0.7s')
             .end();
-    else 
+    else
         move(collapseDiv)
-                .x(-screen.width)
-                .set('background', 'transparent')
-                .duration('0.7s')
+            .x(-screen.width)
+            .set('background', 'transparent')
+            .duration('0.7s')
             .end();
-};  
+};
 
-function Animate() {
-    move('.overlay')
-        .x(0)
-        .duration(3000)
-        .then()
-            move('.preload')
-                .x(4000)
-                .duration(1000)
-                .delay(1000)
-    .end();
-}
+// function Animate() {
+//     move('.overlay')
+//         .x(0)
+//         .duration(3000)
+//         .then()
+//             move('.preload')
+//                 .x(4000)
+//                 .duration(2000)
+//                 .delay(1000)
+//     .end();
+// }
