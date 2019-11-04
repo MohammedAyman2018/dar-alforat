2c09129209e64ab8a1b2bb2d92129f3f01a0f3e6
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-04 19:00:23 +0200

    Fix Navbar responsive problem

--- js/app.js
+++ js/app.js
@@ -6,60 +6,21 @@ window.onload = function (){
 var collapseBtn = document.getElementById('toggle'),
     collapseDiv = document.getElementById('collapseable'),
     nav = document.querySelector('nav'),
-
-    navLinks = document.querySelectorAll('nav .links ul li a:not(.active) , nav i');
-
-window.onscroll = function () {
-    if (window.scrollY <= 100) {
-        navLinks.forEach(link => {
-            link.style.color = "white";
-        });
-
-        move(nav)
-            .set('background', 'transparent')
-            .duration('0.7s')
-            .end();
-        
-        if (screen.width <= 640) {
-            move(collapseDiv)
-                .set('background', 'black')
-                .duration('0.7s')
-                .end()
-        } else {
-            move(collapseDiv)
-                .set('background', 'transparent')
-                .duration('0.7s')
-                .end()
-        }
-
-    } else {
-        navLinks.forEach(link => {
-            link.style.color = "black";
-        });
-        move(nav)
-            .set('background', '#E7E8EA')
-            .duration('0.7s')
-            .end();
-        move(collapseDiv)
-            .set('background', '#E7E8EA')
-            .duration('0.7s')
-            .end()
-    }
-}
-
+    navLinks = document.querySelectorAll('nav .links ul li a:not(.active)'),
+    toogle = document.querySelector('nav i');
 
 collapseBtn.onclick = function () {
     console.log();
     if (collapseDiv.style.transform !== 'translateX(0px)')
         move(collapseDiv)
         .x(0)
-        .set('background', 'black')
+        .set('background', '#E7E8EA')
         .duration('0.7s')
         .end();
     else
         move(collapseDiv)
         .x(-screen.width)
-        .set('background', 'transparent')
+        .set('background', '#E7E8EA')
         .duration('0.7s')
         .end();
 };
---
+++ js/home.js
@@ -0,0 +1,48 @@
+window.onscroll = function () {
+    if (screen.width <= 640) {
+        if (window.scrollY <= 100) {
+            move(nav)
+                .set('background', 'transparent')
+                .duration('0.7s')
+                .end();
+
+
+            move(collapseDiv)
+                .set('background', '#E7E8EA')
+                .duration('0.7s')
+                .end();
+
+            move(toogle).set('color', 'white').duration('0.4s').end();
+            navLinks.forEach(link => {
+                move(link).set('color', 'black').duration('0.4s').end();
+            });
+        } else {
+            move(nav)
+                .set('background', '#E7E8EA')
+                .duration('0.7s')
+                .end();
+
+            move(toogle).set('color', 'black').duration('0.4s').end();
+        }
+    } else {
+        if (window.scrollY <= 100) {
+            move(nav)
+                .set('background', 'transparent')
+                .duration('0.7s')
+                .end();
+
+            navLinks.forEach(link => {
+                move(link).set('color', 'white').duration('0.4s').end();
+            });
+        } else {
+            move(nav)
+                .set('background', '#E7E8EA')
+                .duration('0.7s')
+                .end();
+
+            navLinks.forEach(link => {
+                move(link).set('color', 'black').duration('0.4s').end();
+            });
+        }
+    }
+};
