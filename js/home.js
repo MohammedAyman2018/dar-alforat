0174f508c399bc3fcc2e05b6950124100ef87fdd
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-04 21:36:28 +0200

    Add AllBooks & Books pages

--- js/home.js
+++ js/home.js
@@ -1,3 +1,23 @@
+if (window.scrollY <= 100) {
+    move(nav)
+        .set('background', 'transparent')
+        .duration('0.7s')
+        .end();
+
+    navLinks.forEach(link => {
+        move(link).set('color', 'white').duration('0.4s').end();
+    });
+} else {
+    move(nav)
+        .set('background', '#E7E8EA')
+        .duration('0.7s')
+        .end();
+
+    navLinks.forEach(link => {
+        move(link).set('color', 'black').duration('0.4s').end();
+    });
+}
+
 window.onscroll = function () {
     if (screen.width <= 640) {
         if (window.scrollY <= 100) {
