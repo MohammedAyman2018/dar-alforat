968f247ac3a4f67d24589a31945db80ef0e96f20
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-07 16:53:54 +0200

    Add npm & postcss

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './layouts/**/*.html'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ]
+}
