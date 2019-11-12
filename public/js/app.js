f1266d7bdae267b7fb39409a30d4236cbba579fa
MohammedAyman2018 <emidobibo20@gmail.com>
2019-11-12 22:16:23 +0200

    Client Edits

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-window.onload = function (){
-    Animate();    
+window.onload = function () {
+    Animate();
     ScrollOut();
 }
 
@@ -10,19 +10,18 @@ var collapseBtn = document.getElementById('toggle'),
     toogle = document.querySelector('nav i');
 
 collapseBtn.onclick = function () {
-    console.log();
     if (collapseDiv.style.transform !== 'translateX(0px)')
         move(collapseDiv)
-        .x(0)
-        .set('background', '#E7E8EA')
-        .duration('0.7s')
-        .end();
+            .x(0)
+            .set('background', '#E7E8EA')
+            .duration('0.7s')
+            .end();
     else
         move(collapseDiv)
-        .x(-screen.width)
-        .set('background', '#E7E8EA')
-        .duration('0.7s')
-        .end();
+            .x(-screen.width)
+            .set('background', '#E7E8EA')
+            .duration('0.7s')
+            .end();
 };
 
 function Animate() {
@@ -30,9 +29,9 @@ function Animate() {
         .x(0)
         .duration(3000)
         .then()
-            move('.preload')
-                .x(4000)
-                .duration(2000)
-                .delay(1000)
-    .end();
+    move('.preload')
+        .x(4000)
+        .duration(2000)
+        .delay(1000)
+        .end();
 }
--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,19 @@
+document.getElementById('toggle').style.color = "white";
+
 if (window.scrollY <= 100) {
     move(nav)
         .set('background', 'transparent')
         .duration('0.7s')
         .end();
-
-    navLinks.forEach(link => {
-        move(link).set('color', 'white').duration('0.4s').end();
-    });
+        if (screen.width <= 640) {
+            navLinks.forEach(link => {
+                move(link).set('color', 'black').duration('0.4s').end();
+            });
+        }else {
+            navLinks.forEach(link => {
+                move(link).set('color', 'white').duration('0.4s').end();
+            });
+        }
 } else {
     move(nav)
         .set('background', '#E7E8EA')
